bbf1823fd60ae34c08d515f26303ae683c57dcc1
Berkay <berkaysutlu94@gmail.com>
2023-11-01 01:31:52 +0300

    Initial Commit

---
+++ Ravenna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ravenna
+//
+//  Created by Berkay Sutlu on 1.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ravenna/RavennaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavennaApp.swift
+//  Ravenna
+//
+//  Created by Berkay Sutlu on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RavennaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
